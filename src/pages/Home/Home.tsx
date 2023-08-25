605a620dcb60b6ceef058f69eff757738d8b74e5
naveen <singhnaveen74203@gmail.com>
2023-08-25 16:26:10 +0530

    some code changed

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -62,10 +62,7 @@ const Home = () => {
 
 </div>
 
-{/* <StudentIdCard/> */}
-
-    
-    <div className={styles.footerWrapper}>
+<div className={styles.footerWrapper}>
 <div className={styles.footerDetails}><h1>Ashoka</h1> <span>, All Right CopyRight 2023</span>
  </div>
 <div className={styles.footercopyRight}>
