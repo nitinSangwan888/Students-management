7d8be05f7cbec11a8361671cd7b2c13e159f4ca8
naveen <singhnaveen74203@gmail.com>
2023-08-25 15:16:30 +0530

    some add

--- src/components/Calender/Calendar.tsx
+++ src/components/Calender/Calendar.tsx
@@ -1,17 +1,20 @@
-import * as React from 'react';
+
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import Card from '@mui/material/Card';
+
 import './Calendar.css'
 export default function BasicDateCalendar() {
   return (
-    <div style={{backgroundColor:`var(--light)`,color:`var(--dark)` ,width:"32%"}}>
+    <Card variant="outlined" sx={{backgroundColor:`var(--light)`,color:`var(--dark)`,width:"32%", height:"52vh"}} >
+
 
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DateCalendar />
 
     </LocalizationProvider>
-    </div>
+    </Card>
 
   );
 }
